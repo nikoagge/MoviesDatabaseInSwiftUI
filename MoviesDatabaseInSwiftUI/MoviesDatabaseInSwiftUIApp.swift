f6847eb5eaf9cafa7c88d12b6389679fdfef2d19
nikoagge <nikoagge@gmail.com>
2023-07-15 18:39:51 +0300

    Initial commit

---
+++ MoviesDatabaseInSwiftUI/MoviesDatabaseInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesDatabaseInSwiftUIApp.swift
+//  MoviesDatabaseInSwiftUI
+//
+//  Created by Nikos Aggelidis on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesDatabaseInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
