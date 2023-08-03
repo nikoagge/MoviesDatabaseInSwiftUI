bc1975f306648f9604d18e148f00577ebca0dc0c
nikoagge <nikoagge@gmail.com>
2023-08-03 13:55:04 +0300

    Created SearchBarView

---
+++ MoviesDatabaseInSwiftUI/Views/MovieSearchView.swift
@@ -0,0 +1,24 @@
+//
+//  MovieSearchView.swift
+//  MoviesDatabaseInSwiftUI
+//
+//  Created by Nikos Aggelidis on 2/8/23.
+//
+
+import SwiftUI
+
+struct MovieSearchView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("data")
+            }
+        }
+    }
+}
+
+struct MovieSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieSearchView()
+    }
+}
---
+++ MoviesDatabaseInSwiftUI/Views/UIKitViews/SearchBarView.swift
@@ -0,0 +1,47 @@
+//
+//  SearchBarView.swift
+//  MoviesDatabaseInSwiftUI
+//
+//  Created by Nikos Aggelidis on 2/8/23.
+//
+
+import SwiftUI
+
+struct SearchBarView: UIViewRepresentable {
+    @Binding var text: String
+    let placeholder: String
+    
+    func updateUIView(_ uiView: UISearchBar, context: Context) {
+        uiView.text = text
+    }
+    
+    func makeUIView(context: Context) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.placeholder = placeholder
+        searchBar.searchBarStyle = .minimal
+        searchBar.enablesReturnKeyAutomatically = false
+        searchBar.delegate = context.coordinator
+        
+        return searchBar
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(text: $text)
+    }
+    
+    class Coordinator: NSObject, UISearchBarDelegate {
+        @Binding var text: String
+        
+        init(text: Binding<String>) {
+            _text = text
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            self.text = searchText
+        }
+        
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.resignFirstResponder()
+        }
+    }
+}
